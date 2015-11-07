4541da0babc071ca65540f261cad36cd85c57452
Justin Wolford <wolfordj@oregonstate.edu>
2015-11-07 06:12:34 +0000

    Add hello-express and git ignore

---
+++ hello-express/helloWorld.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', 3000);
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
