f66c2ef45ad7fb9e759b5bb1a9f0d9c5fc987ff4
Justin Wolford <jdwolford@gmail.com>
2016-03-27 21:59:33 -0700

    Require port as command line argument

--- diagnostic/diagnostic.js
+++ diagnostic/diagnostic.js
@@ -6,7 +6,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', process.argv[2]);
 
 app.get('/',function(req,res,next){
   var context = {};
